c30f8e1ab1564ee2ef857ca4a9d260b547cda6c7
Sean Coffey <sean@coffeyhouse.com>
2015-11-21 17:12:05 -0500

    Giraffe Neck

--- GamJam/Assets/Scripts/Grapple.cs
+++ GamJam/Assets/Scripts/Grapple.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Grapple : MonoBehaviour {
+public class Grapple : MonoBehaviour
+{
+    public float force;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * force, ForceMode2D.Impulse);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+    {
+	    
 	}
+
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.tag == "grapple")
+        {
+            transform.parent = hit.transform;
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
 }
--- GamJam/Assets/Scripts/PlayerController.cs
+++ GamJam/Assets/Scripts/PlayerController.cs
@@ -3,12 +3,56 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    float movePower;
+    public float movePower;
+
+    public GameObject grappleFab;
+
+    private float fx;
+    private bool grappling = false;
+    private GameObject grappleActive;
 	
 	// Update is called once per frame
 	void Update ()
     {
-        float fx = Input.GetAxis("Horizontal");
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(fx, 0));
+        CheckInput();
 	}
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(fx * movePower, 0));
+
+        if (grappling)
+        {
+            GetComponent<LineRenderer>().SetPosition(0, transform.position);
+            GetComponent<LineRenderer>().SetPosition(1, grappleActive.transform.position);
+            GetComponent<LineRenderer>().material.mainTextureScale = new Vector2(Vector2.Distance(grappleActive.transform.position, transform.position), 1);
+        }
+    }
+
+    private void CheckInput()
+    {
+        fx = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (grappling)
+            {
+                Destroy(grappleActive);
+                grappling = false;
+                GetComponent<LineRenderer>().enabled = false;
+                GetComponent<LineRenderer>().SetPosition(0, transform.position);
+                GetComponent<LineRenderer>().SetPosition(1, transform.position);
+            }
+            else
+            {
+                Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+                Vector3 direction = Input.mousePosition - pos;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                grappleActive = (GameObject)Instantiate(grappleFab, transform.position, Quaternion.AngleAxis(angle - 90f, Vector3.forward));
+
+                grappling = true;
+                GetComponent<LineRenderer>().enabled = true;
+            }
+        }
+    }
 }
