0c88e8e52a9e93035142bd9278ae9dceeeb1e13c
Sean Coffey <sean@coffeyhouse.com>
2015-11-21 16:06:51 -0500

    Initial Commit

---
+++ GamJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    float movePower;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float fx = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(fx, 0));
+	}
+}
