361689d358fe21b55e452b0bd3824f6432c80e7b
Jonathan Flinchum <jonflinchum@gmail.com>
2015-11-22 12:48:09 -0500

    Score text changes as you get score. Neato

--- GamJam/Assets/Scripts/GameManager.cs
+++ GamJam/Assets/Scripts/GameManager.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityStandardAssets.ImageEffects;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-
+	
 	public int totalScore; //Score of presents + Max distance gone 
 	public int numPresents = 0; //Score of presents
+	public GameObject scoreText;
 	float maxDistance; //Max distance gone
 
 
 	// Update is called once per frame
 	void FixedUpdate () {
+		scoreText.GetComponent<Text>().text = "Score: " + totalScore;
 		if (transform.position.x > maxDistance) {
 			maxDistance = transform.position.x;
 		}
-
 		totalScore = numPresents*100 + (int)maxDistance*20;
 	}
 
