64b243fecd7df4dd15cbb95a76209a35d315cf70
Sean Coffey <sean@coffeyhouse.com>
2015-11-22 00:58:51 -0500

    Farewell Giraffe. I will dismember you fondly.

---
+++ GamJam/Assets/Scripts/LimbScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimbScript : MonoBehaviour
+{
+    float maxVelocity;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag != "Player")
+        {
+            GetComponent<HingeJoint2D>().enabled = false;
+            transform.parent = null;
+        }
+    }
+}
