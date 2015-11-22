ebb7ff9f90f4bcc8cf17f24635de0f0dc6c1619a
Jonathan Flinchum <jonflinchum@gmail.com>
2015-11-21 23:22:00 -0500

    Scoring system started

---
+++ GamJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public int totalScore; //Score of presents + Max distance gone 
+	int numPresents; //Score of presents
+	float maxDistance; //Max distance gone
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (transform.position.x > maxDistance) {
+			maxDistance = transform.position.x;
+		}
+
+		totalScore = numPresents + (int)maxDistance*20;
+	}
+}
---
+++ GamJam/Assets/Scripts/NeckMenuScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeckMenuScript : MonoBehaviour {
+
+	public GameObject neckStart;
+	public GameObject neckEnd;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		GetComponent<LineRenderer>().SetPosition(0, neckStart.transform.position);
+		GetComponent<LineRenderer>().SetPosition(1, neckEnd.transform.position);
+		GetComponent<LineRenderer>().material.mainTextureScale = new Vector2(Vector2.Distance(neckEnd.transform.position, neckStart.transform.position), 1);
+	}
+}
