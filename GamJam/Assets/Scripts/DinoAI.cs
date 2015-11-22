ba67500f63a73c754efdeff27d9e6b7967e8ef48
Jonathan Flinchum <jonflinchum@gmail.com>
2015-11-22 02:41:56 -0500

    Laser follows giraffe... not much else.

--- GamJam/Assets/Scripts/DinoAI.cs
+++ GamJam/Assets/Scripts/DinoAI.cs
@@ -4,13 +4,21 @@ using System.Collections;
 public class DinoAI : MonoBehaviour {
 
 	public GameObject player;
-	public GameObject laser;
+	public GameObject target;
+	public GameObject laserStart;
 	public int timeScaling;
+	private bool shooting;
 	private float playerDistance;
 
 	// Use this for initialization
 	void Start () {
 	
+	}
+
+	void Update(){
+		GetComponent<LineRenderer>().SetPosition(0, laserStart.transform.position);
+		GetComponent<LineRenderer>().SetPosition(1, 50*(player.transform.position - laserStart.transform.position).normalized + laserStart.transform.position);
+		GetComponent<LineRenderer>().material.mainTextureScale = new Vector2(Vector2.Distance(player.transform.position, laserStart.transform.position), 1);
 	}
 	
 	// Update is called once per frame
