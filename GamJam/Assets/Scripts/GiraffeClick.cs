25e9aba96b53b24b5e634c905873349129e2a67e
Sean Coffey <sean@coffeyhouse.com>
2015-11-22 13:44:46 -0500

    Dragraffe

---
+++ GamJam/Assets/Scripts/GiraffeClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiraffeClick : MonoBehaviour
+{
+    private bool locked;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0) && GetComponent<Collider2D>().bounds.Contains(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
+        {
+            locked = true;
+        }
+
+        if (Input.GetMouseButtonUp(0) && locked)
+        {
+            locked = false;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (locked)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+}
