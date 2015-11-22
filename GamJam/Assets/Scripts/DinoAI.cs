4e685153adbf5e94cac84d8207f5ae2669f0d284
Jonathan Flinchum <jonflinchum@gmail.com>
2015-11-22 01:34:20 -0500

    Added Dino movement

---
+++ GamJam/Assets/Scripts/DinoAI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DinoAI : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject laser;
+	public int timeScaling;
+	private float playerDistance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		playerDistance = -1*(transform.position.x - player.transform.position.x);
+		gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (3 + playerDistance/5 + Time.deltaTime/timeScaling, 0);
+	}
+}
