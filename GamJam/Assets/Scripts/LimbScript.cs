cee6e8cc5a4d747c520b0762c056017737d4e941
Sean Coffey <sean@coffeyhouse.com>
2015-11-22 01:03:32 -0500

    Less delicate limbos

--- GamJam/Assets/Scripts/LimbScript.cs
+++ GamJam/Assets/Scripts/LimbScript.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class LimbScript : MonoBehaviour
 {
-    float maxVelocity;
+    public float maxVelocity;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag != "Player")
+        Debug.Log(collision.relativeVelocity.magnitude);
+
+        if (collision.collider.tag != "Player" && collision.relativeVelocity.magnitude > maxVelocity)
         {
             GetComponent<HingeJoint2D>().enabled = false;
             transform.parent = null;
